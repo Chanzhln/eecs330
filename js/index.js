bafb95146c60f1f24b189f7eef02ffc2c428c186
Chanzhln <zhilinchen2015@u.northwestern.edu>
2016-02-28 18:22:39 -0600

    new version by chen

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function jump()
+{	
+	window.location.href="../html/event.html";
+}
+
+function sign_in()
+{
+	window.location.href="../html/sign_in.html";
+}
+
+
+function sign_up()
+{
+	window.location.href="../html/sign_up.html";
+}
+
